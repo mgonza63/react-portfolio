1d2b2104393be19823cb24644cba604a4f948943
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-05-07 23:19:03 -0500

    better readme

---
+++ src/components/Techs/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './style.css'
+
+const Techs = () => {
+    return (
+        <div className="container">
+            
+            <div className="row justify-content-md-center">
+                <div className="col">
+                    <h2 className="mt-5">Technologies I have worked with</h2>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Techs
